f123d1cb968ced9c7fff6f91d0db910484b97eb5
Ramon Souza <ramon291997@gmail.com>
2020-07-09 21:58:53 -0300

    Start of the structure of toasts

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import React from 'react';
 // import SignUp from './pages/Signup';
 import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
-import { AuthProvider } from './context/AuthContext';
+import ToastContainer from './components/ToastContainer';
+import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
   <>
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+
+    <ToastContainer />
 
     <GlobalStyle />
   </>
